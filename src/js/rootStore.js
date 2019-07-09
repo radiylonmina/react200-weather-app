e9f5dec66f69d5621cb4d4e722363aca07d7c44e
radiylonmina <radiylon@gmail.com>
2019-07-09 13:32:43 -0700

    fix environment variables, update .gitignore

--- src/js/rootStore.js
+++ src/js/rootStore.js
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import promiseMiddleware from 'redux-promise-middleware'
+import promise from 'redux-promise-middleware'
 import rootReducer from './rootReducer'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const rootStore = createStore(rootReducer, composeEnhancers(applyMiddleware(promiseMiddleware())));
+const rootStore = createStore(rootReducer, composeEnhancers(applyMiddleware(promise)));
 
 export default rootStore;
